e2b418d03567553f223a84b1ba72552f01dc648d
NgoanMc <phongnhadn1991@gmail.com>
2024-03-31 13:02:01 +0700

    fixuser

--- src/app/_components/common/header/Header.js
+++ src/app/_components/common/header/Header.js
@@ -137,6 +137,7 @@ const Header = () => {
                                                 <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 13.5h3.86a2.25 2.25 0 012.012 1.244l.256.512a2.25 2.25 0 002.013 1.244h3.218a2.25 2.25 0 002.013-1.244l.256-.512a2.25 2.25 0 012.013-1.244h3.859m-19.5.338V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 00-2.15-1.588H6.911a2.25 2.25 0 00-2.15 1.588L2.35 13.177a2.25 2.25 0 00-.1.661z" />
                                             </svg>
                                             <span className='text-[13px] font-normal'>Inbox</span>
+                                            <span className='text-green-500 bg-gray-100 text-[10px] font-medium p-1.5 rounded-sm leading-none ml-auto'>25</span>
                                         </Link>
                                         <Link href="#" className='flex items-center hover:bg-blue-50 gap-2 p-2.5'>
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
